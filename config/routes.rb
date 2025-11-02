6d0b377ab11baa06a13eca88e82c0e8714013140
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-11-02 01:58:33 +0000

    フォローフォロワー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     devise_for :users
     root to: "homes#top"
@@ -8,7 +9,11 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships,only: [:create, :destroy]
+    get 'followings' => 'users#followings', as: 'followings'
+    get 'followers' => 'users#followers', as: 'followers'
+  end
     
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
